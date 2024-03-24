d66fbd068b617309117ff8042839af5896fda0d6
HaveTime.DrinkTea <havetimedrinktea@gmail.com>
2024-03-24 09:30:06 +0000

    set up xcode

---
+++ GuruAndMouse/GuruAndMouse/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GuruAndMouse
+//
+//  Created by Pei Siong WANG on 24/03/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
